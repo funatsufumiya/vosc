4323e22fe8b4da8b3bf9fdc23ddb1ae131473861
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-11-28 14:42:34 +0900

    feat: add sender example

--- examples/example_sender/main.v
+++ examples/example_sender/main.v
@@ -1,5 +1,23 @@
 import vosc
+import net
 
 fn main() {
-	println("hello")
+	raddr := '127.0.0.1:9000'
+	mut sender := net.dial_udp(raddr)!
+	defer {
+		sender.close() or { panic(err) }
+	}
+
+	mut msg := vosc.OscMessage{}
+    msg.address = "/hello"
+    msg.args = [1, f32(2.3), "world"]
+
+    mut buf := []u8{cap: 128}
+    vosc.add_message(mut buf, msg)
+
+	sender.write_ptr(buf.data, int(buf.len))!
+
+	println('OSC sent to ${raddr}.')
+	println("")
+	println("The content is ${msg}")
 }
