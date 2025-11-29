7c4f31add385732f81e361d9f910e75b13c5b8af
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-11-29 10:56:38 +0900

    feat: implement receiver example

--- examples/example_receiver/main.v
+++ examples/example_receiver/main.v
@@ -1,5 +1,37 @@
+module main
+
+import net
 import vosc
 
 fn main() {
-	println("[Error] Sorry, this example is work in progress...")
+	raddr := '0.0.0.0:9000'
+	mut receiver := net.listen_udp(raddr)!
+  defer {
+    receiver.close() or { panic(err) }
+  }
+	println('OSC listening to ${raddr}')
+
+	mut buf := []u8{len: 2048}
+	for {
+		len, _ := receiver.read(mut buf) or { continue }
+
+		packet := vosc.read_packet(buf)!
+
+		// print(packet)
+
+		if packet.kind == .message {
+			msg := packet.msg
+			print(msg)
+		} else if packet.kind == .bundle {
+			bundle := packet.bundle
+			t := vosc.to_time(bundle.time)
+			print(t)
+			for pac in bundle.contents {
+				if pac.kind == .message {
+					msg := pac.msg
+					print(msg)
+				}
+			}
+		}
+	}
 }
